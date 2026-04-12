957a16a23c1d875b1abfc76704617d75b165f6bb
FlyInMyEye <69flyinmyeye69@gmail.com>
2026-04-12 10:38:07 +0200

    Blueprint compat

---
+++ src/main/java/net/fly/adrenaline/compat/blueprint/MixinBlueprintTreeFeature.java
@@ -0,0 +1,23 @@
+package net.fly.adrenaline.compat.blueprint;
+
+import com.teamabnormals.blueprint.common.levelgen.feature.BlueprintTreeFeature;
+import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(BlueprintTreeFeature.class)
+public class MixinBlueprintTreeFeature {
+
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    @Inject(method = "place", at = @At("HEAD"), cancellable = true)
+    private void syncPlace(FeaturePlaceContext ctx, CallbackInfoReturnable<Boolean> cir) {
+        if (!Thread.holdsLock(this)) {
+            synchronized (this) {
+                cir.setReturnValue(((Feature) (Object) this).place(ctx));
+            }
+        }
+    }
+}
