56e487867296bccaad9d5c92f75518a0771009c2
FlyInMyEye <69flyinmyeye69@gmail.com>
2026-07-27 19:14:34 +0200

    isolate client-only initialization

--- src/main/java/net/fly/adrenaline/Adrenaline.java
+++ src/main/java/net/fly/adrenaline/Adrenaline.java
@@ -1,21 +1,14 @@
 package net.fly.adrenaline;
 
-import net.fly.adrenaline.client.AdrenalineConfigScreen;
-import net.fly.adrenaline.client.BuildWarningOverlay;
-import net.fly.adrenaline.client.FastloadCompatController;
-import net.fly.adrenaline.client.ModernFixCompatController;
-import net.fly.adrenaline.client.WorldgenStatsOverlay;
-import net.fly.adrenaline.compatdata.IncompatibilityRegistry;
+import net.fly.adrenaline.client.AdrenalineClient;
 import net.fly.adrenaline.config.AdrenalineConfig;
 import net.fly.adrenaline.scheduler.ChunkJobScheduler;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.ConfigScreenHandler;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.loading.FMLEnvironment;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -27,22 +20,7 @@ public class Adrenaline {
 
     public Adrenaline() {
         AdrenalineConfig.init();
-        if (FMLEnvironment.dist == Dist.CLIENT) {
-            ModLoadingContext.get().registerExtensionPoint(
-                ConfigScreenHandler.ConfigScreenFactory.class,
-                () -> new ConfigScreenHandler.ConfigScreenFactory((minecraft, screen) -> new AdrenalineConfigScreen(screen))
-            );
-
-            MinecraftForge.EVENT_BUS.register(new ModernFixCompatController());
-            if (BuildConfig.DEBUG) {
-                MinecraftForge.EVENT_BUS.register(new BuildWarningOverlay());
-                MinecraftForge.EVENT_BUS.register(new WorldgenStatsOverlay());
-            }
-
-            FastloadCompatController fastloadCompatController = new FastloadCompatController();
-            fastloadCompatController.setShouldShow(IncompatibilityRegistry.isLoaded("fastload"));
-            MinecraftForge.EVENT_BUS.register(fastloadCompatController);
-        }
+        DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> AdrenalineClient::initialize);
         MinecraftForge.EVENT_BUS.register(this);
     }
 
