0ac08509793fe8ef5513159b4bfe276e8a537b3d
FlyInMyEye <69flyinmyeye69@gmail.com>
2026-08-01 15:27:12 +0200

    add config button to world creation

--- src/main/java/net/fly/adrenaline/mixin/MixinCreateWorldScreen.java
+++ src/main/java/net/fly/adrenaline/mixin/MixinCreateWorldScreen.java
@@ -4,25 +4,49 @@ import java.util.Optional;
 import java.util.OptionalLong;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
+import net.fly.adrenaline.client.AdrenalineConfigScreen;
 import net.fly.adrenaline.client.WorldCreationContextWaiter;
 import net.fly.adrenaline.config.AdrenalineConfig;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.layouts.FrameLayout;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.worldselection.CreateWorldScreen;
 import net.minecraft.client.gui.screens.worldselection.WorldCreationContext;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.WorldLoader;
 import net.minecraft.world.level.levelgen.presets.WorldPreset;
 import net.minecraft.world.level.levelgen.presets.WorldPresets;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(CreateWorldScreen.class)
-public class MixinCreateWorldScreen {
+public abstract class MixinCreateWorldScreen extends Screen {
+
+    @Unique
+    private Button adrenaline$configButton;
+
+    protected MixinCreateWorldScreen(Component title) {
+        super(title);
+    }
+
+    @Inject(method = "init", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/worldselection/CreateWorldScreen;repositionElements()V"))
+    private void adrenaline$addConfigButton(CallbackInfo ci) {
+        this.adrenaline$configButton = this.addRenderableWidget(Button.builder(Component.literal("A").setStyle(Style.EMPTY.withColor(0xFF59BD)), button -> this.minecraft.setScreen(new AdrenalineConfigScreen(this))).bounds(0, 0, 20, 20).build());
+    }
+
+    @Inject(method = "repositionElements", at = @At("TAIL"))
+    private void adrenaline$positionConfigButton(CallbackInfo ci) {
+        if (this.adrenaline$configButton != null) {
+            FrameLayout.alignInRectangle(this.adrenaline$configButton, 4, this.height - 36, 20, 36, 0.0F, 0.5F);
+        }
+    }
 
     @Inject(method = "queueLoadScreen", at = @At("HEAD"), cancellable = true)
     private static void adrenaline$suppressPreloadMessage(Minecraft minecraft, Component message, CallbackInfo ci) {
