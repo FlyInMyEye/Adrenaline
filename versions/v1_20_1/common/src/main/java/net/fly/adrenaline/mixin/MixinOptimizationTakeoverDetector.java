3d8efb30b6456bc2e5b9b300164ac09fd623f074
FlyInMyEye <69flyinmyeye69@gmail.com>
2026-08-25 11:07:47 +0200

    inline improved noise lattice sampling

--- versions/v1_20_1/common/src/main/java/net/fly/adrenaline/mixin/MixinOptimizationTakeoverDetector.java
+++ versions/v1_20_1/common/src/main/java/net/fly/adrenaline/mixin/MixinOptimizationTakeoverDetector.java
@@ -6,8 +6,9 @@ import net.minecraft.world.level.levelgen.Beardifier;
 import net.minecraft.world.level.levelgen.NoiseBasedChunkGenerator;
 import net.minecraft.world.level.levelgen.NoiseChunk;
 import net.minecraft.world.level.levelgen.SurfaceSystem;
+import net.minecraft.world.level.levelgen.synth.ImprovedNoise;
 import org.spongepowered.asm.mixin.Mixin;
 
-@Mixin(value = {NoiseBasedChunkGenerator.class, NoiseChunk.class, Aquifer.NoiseBasedAquifer.class, SurfaceSystem.class, Beardifier.class, ChunkMap.class}, priority = 500)
+@Mixin(value = {NoiseBasedChunkGenerator.class, NoiseChunk.class, Aquifer.NoiseBasedAquifer.class, SurfaceSystem.class, Beardifier.class, ChunkMap.class, ImprovedNoise.class}, priority = 500)
 public class MixinOptimizationTakeoverDetector {
 }
